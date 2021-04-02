519162588e4a0718ce9123cb3ac5e47ffb48e344
Yahya Allam <yallam122@gmail.com>
2021-04-02 14:53:05 +0200

    solve the problem of composite key in purchase table.

--- src/main/java/eg/gov/iti/jets/model/Purchase.java
+++ src/main/java/eg/gov/iti/jets/model/Purchase.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -17,17 +18,19 @@ import java.util.Set;
 public class Purchase implements Serializable {
 
     @EmbeddedId
-    private PurchaseId purchasesId ;
+    private PurchaseId purchasesId = new PurchaseId();
 
     @Column(name = "QUANTITY")
     private int quantity;
 
 
     @ManyToOne
+    @MapsId("orderId")
     @JoinColumn(name = "ORDER_ID",insertable = false,updatable = false)
     private Order order ;
 
     @ManyToOne
+    @MapsId("productId")
     @JoinColumn(name = "PRODUCT_ID",insertable = false,updatable = false)
     private Product product;
 
@@ -39,4 +42,5 @@ public class Purchase implements Serializable {
         order.getPurchase().add(this);
         product.getPurchase().add(this);
     }
+
 }
