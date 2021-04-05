67e6be4edd790d9bbb0440dda8d6710e014513c1
Mahmoud Ali Khalil <mahmoud.alikhalil@gmail.com>
2021-04-05 20:48:15 +0200

    Add Shopping cart Client side.

--- src/main/webapp/js/ajax-shoppingCart.js
+++ src/main/webapp/js/ajax-shoppingCart.js
@@ -19,12 +19,12 @@ $(function () {
             .done(function (response) {
                 console.log(response);
                 $("#shoppingCardContainer").append(`<li>
-                                                        <a href="single-product.jsp" class="minicart-product-image">
+                                                        <a href="productInfo?productId=${response.productId}" class="minicart-product-image">
                                                             <img src="${response.productImage.imagePath}"
                                                                 alt="cart products">
                                                         </a>
                                                         <div class="minicart-product-details">
-                                                            <h6><a href="single-product.jsp">${response.productName}</a></h6>
+                                                            <h6><a href="productInfo?productId=${response.productId}">${response.productName}</a></h6>
                                                             <span>$${response.price}</span>
                                                         </div>
                                                         <button class="close removeFromCart" title="Remove" data-product="${response.productId}" onclick="removeElement(this)">
@@ -32,7 +32,7 @@ $(function () {
                                                         </button>
                                                     </li>`);
             });
-        $("#shopCartCounter").text($("#shopCartCounter").text() + 1);
+        $("#shopCartCounter").text(Number.parseInt($("#shopCartCounter").text()) + 1);
     });
 
 });
@@ -43,16 +43,16 @@ function removeElement(element) {
         url: 'removePurchase',
         data: {"orderedProductId": $(element).data("product")}
     }).done(function (response) {
-        $("#shoppingCardContainer").clear();
+        $("#shoppingCardContainer").empty();
         response.itemsOrdered.forEach(e => {
             let productData = e.orderedProductDTO;
             $("#shoppingCardContainer").append(`<li>
-                                                        <a href="single-product.jsp" class="minicart-product-image">
+                                                        <a href="productInfo?productId=${productData.productId}" class="minicart-product-image">
                                                             <img src="${productData.productImage.path}"
                                                                 alt="cart products">
                                                         </a>
                                                         <div class="minicart-product-details">
-                                                            <h6><a href="single-product.jsp">${productData.productName}</a></h6>
+                                                            <h6><a href="productInfo?productId=${productData.productId}">${productData.productName}</a></h6>
                                                             <span>$${productData.price}</span>
                                                         </div>
                                                         <button class="close removeFromCart" title="Remove" data-product="${productData.productId}" onclick="removeElement(this)">
